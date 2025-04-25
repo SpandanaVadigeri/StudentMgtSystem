b9e0ca287aab3a8d1888bba2f25ebe18609087f6
sumangalasipoy@gmail.com <sumangalasipoy@gmail.com>
2025-04-25 14:19:20 +0700

    Added a function view and search

--- Main.java
+++ Main.java
@@ -40,3 +40,25 @@ public class Main {
  
         } while (choice != 5); 
     }
+
+    static void viewStudents() {
+        if (students.isEmpty()) {
+        System.out.println("No students found.");
+        return;
+        }
+        System.out.println("\n--- List of Students ---");
+        for (Student s : students) {
+        System.out.println(s);
+        }
+        }
+        static void searchStudent() {
+        System.out.print("Enter Student ID to search: ");
+        int id = sc.nextInt();
+        for (Student s : students) {
+        if (s.getId() == id) {
+        System.out.println("Student Found: " + s);
+        return;
+        }
+        }
+        System.out.println("Student with ID " + id + " not found.");
+    }        
