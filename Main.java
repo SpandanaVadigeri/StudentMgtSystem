1967b16abb269fdcd5f9f45d03c0d1808d9aff4e
SpandanaVadigeri <spandanavadigeri@gmail.com>
2025-04-25 15:54:08 +0530

    Updated Main.java

--- Main.java
+++ Main.java
@@ -35,24 +35,7 @@ public class Main {
             } 
         } while (choice != 5); 
     }
-// delete-student
-static void deleteStudent() {
-System.out.print("Enter Student ID to delete: ");
-int id = sc.nextInt();
-Iterator<Student> iterator = students.iterator();
-while (iterator.hasNext()) {
-Student s = iterator.next();
-if (s.getId() == id) {
-iterator.remove();
-System.out.println("Student with ID " + id + " deleted successfully.");
-return;
-}
-}
-}
-System.out.println("Student with ID " + id + " not found.");
-}
-}
- static void addStudent() {
+    static void addStudent() {
         System.out.print("Enter Student ID: ");
         int id = sc.nextInt();
         sc.nextLine(); // Clear buffer
@@ -62,27 +45,43 @@ System.out.println("Student with ID " + id + " not found.");
         String grade = sc.nextLine();
         students.add(new Student(id, name, grade));
         System.out.println("Student added successfully.");
-        }
-
- static void viewStudents() {
+    }
+     static void viewStudents() {
         if (students.isEmpty()) {
-        System.out.println("No students found.");
-        return;
+            System.out.println("No students found.");
+            return;
         }
         System.out.println("\n--- List of Students ---");
         for (Student s : students) {
-        System.out.println(s);
+            System.out.println(s);
         }
-        }
-        static void searchStudent() {
+    }
+    static void searchStudent() {
         System.out.print("Enter Student ID to search: ");
         int id = sc.nextInt();
         for (Student s : students) {
-        if (s.getId() == id) {
-        System.out.println("Student Found: " + s);
-        return;
-        }
+            if (s.getId() == id) {
+            System.out.println("Student Found: " + s);
+            return;
+            }    
         }
         System.out.println("Student with ID " + id + " not found.");
-        }        
+    }        
+    static void deleteStudent() {
+        System.out.print("Enter Student ID to delete: ");
+        int id = sc.nextInt();
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student s = iterator.next();
+            if (s.getId() == id) {
+                iterator.remove();
+                System.out.println("Student with ID " + id + " deleted successfully.");
+                return;
+            }
+        }
+        System.out.println("Student with ID " + id + " not found.");
+    }
+}
+ 
+
 
